f4743e95fd87d65c56bb74713c5162547339e241
Christopher Garon <christopher.garon@gmail.com>
2021-10-22 14:03:34 -0700

    Added slow walk (Shift of Ctrl)

--- Egg Cup v1/Assets/Scripts/PlayerController.cs
+++ Egg Cup v1/Assets/Scripts/PlayerController.cs
@@ -20,6 +20,7 @@ public class PlayerController : MonoBehaviour
     
     // STATS CONTROLLER
     [SerializeField] private float speed = 7.5f;
+    [SerializeField] private float slowWalkSpeed = 3.5f;
     [SerializeField] private float jumpHeight = 7.5f;
 
 
@@ -30,7 +31,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float knockBack = 10f;
 
     // BUTTON INPUTS
-    private bool leftDown, rightDown, downDown, jumpDown;
+    private bool leftDown, rightDown, downDown, jumpDown, shiftDown;
 
     public bool hasEgg;
 
@@ -81,6 +82,7 @@ public class PlayerController : MonoBehaviour
         rightDown = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) || Input.GetAxisRaw("Horizontal") > 0;
         downDown = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) || Input.GetAxisRaw("Vertical") < 0;
         jumpDown = Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space);
+        shiftDown = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift) || Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
     }
 
     // MOVEMENT 
@@ -93,13 +95,13 @@ public class PlayerController : MonoBehaviour
         // MOVING LEFT + FLIPPING SPRITE
         if(leftDown)
         {
-            velo.x = -speed;
+            velo.x = shiftDown ? -slowWalkSpeed : -speed;
             transform.localScale = new Vector2(-1, 1);
         }
         // MOVING RIGHT + FLIPPING SPRITE
         else if (rightDown)
         {
-            velo.x = speed;
+            velo.x = shiftDown ? slowWalkSpeed : speed;
             transform.localScale = new Vector2(1, 1);
         }
         // STOPPING ON GROUND
